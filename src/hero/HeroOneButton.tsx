2935d6f3262ad38ffc9c29ddfc15137ba4e7f037
Ben Turley <ben@benturley.com>
2024-12-18 14:08:56 -0700

    Add news about OpenAI demo

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -13,6 +13,33 @@ const HeroOneButton = (props: IHeroOneButtonProps) => (
     </h1>
     <div className="text-2xl mt-4 mb-16">{props.description}</div>
 
+    <div className="mb-8 p-4 border border-blue-500 bg-blue-100 text-blue-700 rounded">
+      <div className="text-xl font-bold text-center">
+        Sonatino Featured in OpenAI Live Demo!
+      </div>
+      To show off their new Realtime API, OpenAI used Sonatino in their{' '}
+      <a
+        className="font-semibold text-blue-700"
+        href="https://openai.com/12-days/"
+      >
+        12 Days of OpenAI
+      </a>{' '}
+      live demo.
+      <br />
+      The demo featured a plush reindeer toy (with Sonatino stuffed inside),
+      programmed to listen and respond to spoken questions in real-time. Watch
+      it{' '}
+      <a
+        href="https://youtu.be/14leJ1fg4Pw?si=Sa0rszsIh8P0VvQl&t=796"
+        className="underline text-blue-700"
+        target="_blank"
+        rel="noreferrer"
+      >
+        here
+      </a>
+      .
+    </div>
+
     {props.button}
     <div className="text-xl mt-2 text-red-700 font-bold">
       Currently out of stock, but more are on the way!
